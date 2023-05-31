bbc1c35f37c4347bc997cb8c2bfe0a97fe731e3a
Inna Dmytrenko <inna.shalun@gmail.com>
2023-05-31 20:16:54 +0300

    Update App

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>HOMEWORK 1 </Text>
+      <Text>Inna Dmytrenko</Text>
       <StatusBar style="auto" />
     </View>
   );
