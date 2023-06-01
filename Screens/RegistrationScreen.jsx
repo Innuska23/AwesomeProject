31f1d24d4232c5d2c7974d498df1fee43f90fd6a
Inna Dmytrenko <inna.shalun@gmail.com>
2023-06-01 23:07:17 +0300

    Update RegistrationScreen.jsx

--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -1,27 +1,46 @@
-import React from "react";
-import { Container, ContainerText, Input } from "./RegistrationScreen.styled";
+import React, { useState, useEffect } from "react";
+import { Keyboard } from "react-native";
+import { ButtonRegistation, ButtonShow, ButtonText, Container, ContainerText, FhotoWraper, Input, InputButton, Label, LinkText, PasswordWraper, Photo, ShowButton, Text, WrapperContainer, WrapperImage } from "./RegistrationScreen.styled";
 
-export default RegistrationScreen = () => (
-    <Container>
-        <ContainerText>Реєстрація</ContainerText>
-        <Input placeholder="Логін"/>
-        <Input placeholder="Адреса електронної пошти"/>
-        <Input placeholder="Пароль"/>
-    </Container>
-);
+export default RegistrationScreen = () => {
 
-// const styles = StyleSheet.create({
-//     container: {
-//         width: 375,
-//         height: 549,
-//         backgroundColor: "#FFFFFF"
-//     },
-//     title: {
-//         fontFamily: 'Roboto',
-//         fontStyle: "normal",
-//         fontWeight: 500,
-//         fontSize: 30,
-//         lineHeight: 35,
-//         textAlign: "center",
-//     }
-// });
+    const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+
+    useEffect(() => {
+        const showSubscription = Keyboard.addListener('keyboardDidShow', () => {
+            setIsKeyboardOpen(true);
+        });
+        const hideSubscription = Keyboard.addListener('keyboardDidHide', () => {
+            setIsKeyboardOpen(false);
+        });
+
+        return () => {
+            showSubscription.remove();
+            hideSubscription.remove();
+        };
+    }, []);
+
+    return <WrapperContainer>
+        <WrapperImage source={require('./BG/PhotoBG.png')} resizeMode="cover">
+
+            <Container>
+            <FhotoWraper> 
+                <Photo resizeMode="cover" />
+            </FhotoWraper>
+                <ContainerText>
+                    <Text>Реєстрація</Text>
+                </ContainerText>
+                <Input placeholder="Логін" />
+                <Input placeholder="Адреса електронної пошти" />
+                <PasswordWraper>
+                    <Input placeholder="Пароль" />
+                    <ShowButton><Label>Показати</Label></ShowButton>
+                </PasswordWraper>
+                {!isKeyboardOpen && <><ButtonRegistation>
+                    <ButtonText>Зареєструватись</ButtonText>
+                </ButtonRegistation>
+                    <LinkText>Вже є акаунт? Увійти</LinkText></>}
+            </Container>
+        </WrapperImage>
+    </WrapperContainer>
+};
--- Screens/RegistrationScreen.styled.jsx
+++ Screens/RegistrationScreen.styled.jsx
@@ -1,30 +1,108 @@
 import styled, { css } from '@emotion/native'
 
-export const Container = styled.View`
+export const WrapperContainer = styled.View`
+    font-family: 'Roboto';
     width: 100%;
     height: 100%;
-    ${'' /* display: flex;
-    position: absolute;
-    justify-content: center;
-    align-items: center; */}
-    ${'' /* width: 375,
-    height: 549, */}
-    background-color: ##FFFFFF;
-    padding: 92px 16px 78px 16px; 
+
 `;
 
-export const ContainerText = styled.Text`
-    ${'' /* font-style: normal;
-    font-weight: 500;
-    font-size: 30px;
-    line-height: 35px;
-    text-align: center;
-    letter-spacing: 0.01em; */}
-    color: #212121;
+export const WrapperImage = styled.ImageBackground`
+    display: flex;
+    flex: 1;
+    justify-content: flex-end;
+    position: relative;
+`;
 
+export const FhotoWraper = styled.View`
+    position: absolute;
+    z-index: 1000;
+    left: 140px;
+    top: -60px;
+    flex: 1;
+`;
+
+export const Photo= styled.Image`
+    position: relative;
+    width: 120px;
+    height: 120px;
+    background: #F6F6F6;
+    border-radius: 16px;
+    flex: 1;
+    z-index: 2;
+`;
+
+export const Container = styled.View`
+    position: relative;
+    width: 100%;
+    left: 0px;
+    ${'' /* height: 100%; */}
+    height: 549px;
+    background-color: #FFFFFF;
+    border-radius: 25px 25px 0px 0px;
+    padding: 92px 16px 78px 16px; 
+    z-index: 1;
+
+`;
+
+export const ContainerText = styled.View`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Text = styled.Text`
+    display: flex;
+    color: #212121;
+    font-size: 30px;
+    margin-bottom: 32px;
+    font-weight: 500;
 `;
 
 export const Input = styled.TextInput`
-    background-color: #BDBDBD;
-    width: 100%;
+    &:nth-child(3) {
+        width: 100%;
+        font-weight: 400;
+        line-height: 19px;
+        margin-bottom: 16px;
+        height: 50px;
+        background: #F6F6F6;
+        border: 1px solid #E8E8E8;
+        border-radius: 8px;
+        padding: 16px 15px;
+}
+`;
+export const PasswordWraper = styled.View`
+    position: relative;
+`;
+
+export const ShowButton = styled.TouchableOpacity`
+    position: absolute; 
+    top: 15px;
+    right: 15px;
+`;
+
+export const Label = styled.Text`
+    color: black;
+`;
+
+export const ButtonRegistation = styled.TouchableOpacity`
+    display: flex;
+    background-color: #FF6C00;
+    border-radius: 100px;
+    height: 51px;
+    align-items: center;
+    justify-content: center;
+    margin-top: 43px;
+`;
+
+export const ButtonText = styled.Text`
+    color: #FFFFFF;
+`;
+
+export const LinkText = styled.Text`
+    font-size: 16px;
+    line-height: 19px;
+    text-align: center;
+    margin-top: 16px;
 `;
